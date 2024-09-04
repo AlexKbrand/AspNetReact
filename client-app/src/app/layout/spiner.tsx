5807c9a73f9c15a16b4b6156465a71e1f15493ec
noname068218 <alexriders2000@gmail.com>
2024-09-04 16:52:52 +0200

    dev

---
+++ client-app/src/app/layout/spiner.tsx
@@ -0,0 +1,21 @@
+// import "./spiner.css";
+
+// const HexSpinner = () => {
+//   return (
+//     <div className="spinner-container">
+//       <div className="spinner">
+//         <div></div>
+//         <div></div>
+//         <div></div>
+//         <div></div>
+//         <div></div>
+//         <div></div>
+//         <div></div>
+//         <div></div>
+//         <div></div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default HexSpinner;
--- client-app/src/features/activities/dashboard/ActivityList.tsx
+++ client-app/src/features/activities/dashboard/ActivityList.tsx
@@ -1,3 +1,4 @@
+import { SyntheticEvent, useState } from "react";
 import { Activity } from "../../../app/models/activity";
 import { Segment, Item, Button, Label } from "semantic-ui-react";
 
@@ -5,13 +6,25 @@ interface Props {
   activities: Activity[]; // Replace with your own Activity type definition.
   selectActivity: (id: string) => void;
   deleteActivity: (id: string) => void;
+  submitting: boolean; //
 }
 
 export default function ActivityList({
   activities,
   selectActivity,
   deleteActivity,
+  submitting,
 }: Props) {
+  const [target, setTaget] = useState("");
+
+  function handleActivityDelete(
+    event: SyntheticEvent<HTMLButtonElement>,
+    id: string
+  ) {
+    event.preventDefault();
+    setTaget(event.currentTarget.name);
+    deleteActivity(id);
+  }
   return (
     <Segment>
       <Item.Group divided>
@@ -37,10 +50,12 @@ export default function ActivityList({
                 onClick={() => selectActivity(activity.id)}
               ></Button>
               <Button
-                onClick={() => deleteActivity(activity.id)}
+                name={activity.id}
+                onClick={(e) => handleActivityDelete(e, activity.id)}
                 floated="right"
                 content="Delete"
                 color="red"
+                loading={submitting && target === activity.id}
               />
               <Label basic content={activity.category} />
             </Item.Extra>
